61ac5e0f7ddf125ade5008ea34db0cfeeb543154
Manish Bellani <mbellani@github.com>
2022-05-27 14:07:36 -0500

    replace action with a script

---
+++ script/publish.sh
@@ -0,0 +1,83 @@
+#! /usr/bin/env bash
+#/ Publishes metrics-exporter-statsd to crates.io
+#/ Usage: script/publish --version <version> --token <token> [--dry-run]
+#/ 
+#/ version: Version to publish to crates.io.
+#/ token: cates.io token to use for publishing.
+#/ upload: Actually publishes the the crate to crates.io, by default thisc script 
+#/         runs cargo publish in dry-run mode
+
+set -euo pipefail
+IFS=$'\n\t'
+
+function usage {
+    grep "^#/" "${BASH_SOURCE[0]}" | cut -c 4-
+}
+
+
+PKG_VERSION=$(cargo metadata --format-version 1 | jq  -r '.workspace_members[0]' | awk '{print $2}')
+MODE="--dry-run"
+TOKEN=
+VERSION=
+
+if [[ -z  "$(command -v jq)" ]]; then
+    echo "jq is required to run this script"
+    exit
+fi
+
+
+if [[  "$#" == 0 ]]
+then
+  usage
+  exit
+fi
+# Ensure that the tests are still passing before publishing
+# cargo test --verbose
+
+# parse args
+# extract options and their arguments into variables.
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --help|-h)
+      usage; shift ;;
+    --upload)
+      MODE=; shift ;;
+    --token)
+      TOKEN=$2=1; shift 2;;
+    --version)
+      VERSION=$2; shift 2 ;;
+    --)
+      shift; break ;;
+    *)
+      usage
+      exit
+      ;;
+   esac
+done
+
+if [[ -z "${VERSION}" ]]; then
+    echo "Version is required"
+    exit
+fi
+
+if [[ -z "${TOKEN}" ]]; then
+    echo "Token is required"
+    exit
+fi
+
+if [[ $(git rev-parse --abbrev-ref HEAD) != "main" ]] 
+then
+   echo "You can only publish from main branch"
+   exit
+fi
+
+if [[ "${PKG_VERSION}" != "${VERSION}" ]]; then
+    echo "Please make sure the version matches in the cargo file"
+    exit
+fi
+
+git tag $VERSION
+cargo package 
+cmd="cargo publish ${UPLOAD} --token ${TOKEN}"
+eval $CMD
+git push origin $VERSION
